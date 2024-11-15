aa7288e4fe525e5b8fb107770fe040dc3d044fec
Pallavkumar Jadav <pallavkumar.p.jadav@accenture.com>
2024-11-15 11:55:30 +0000

    S1E4 - Adding Custom Controls - Tiles

--- app/project2/annotations.cds
+++ app/project2/annotations.cds
@@ -1,7 +1,44 @@
 using myempsrv as service from '../../srv/service';
-annotate service.MyEmp with @(
-    UI.SelectionFields #filterBarMacro : [
-    ID,name, dob,erpSystem_erpSystem
-    ]
-);
 
+annotate service.MyEmp with @(
+    UI.SelectionFields #filterBarMacro: [
+        ID,
+        name,
+        dob,
+        erpSystem_erpSystem
+    ],
+    UI.LineItem #tableMacro           : [
+        {
+            $Type: 'UI.DataField',
+            Value: name
+        },
+        {
+            $Type: 'UI.DataField',
+            Value: dob
+        },
+        {
+            $Type: 'UI.DataField',
+            Value: erpSystem_erpSystem
+        },
+        {
+            $Type: 'UI.DataField',
+            Value: client
+        },
+
+    ],
+    UI.HeaderInfo                     : {
+        TypeName      : 'Employee',
+        TypeNamePlural: 'Employees',
+        Title         : {
+            $Type: 'UI.DataField',
+            Value: name,
+        },
+        Description   : {
+            $Type: 'UI.DataField',
+            Value: dob,
+        },
+        // Initials      : name,
+        TypeImageUrl  : 'https://media.licdn.com/dms/image/v2/D4D03AQFI-EkMrmiLzA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699030913771?e=1736985600&v=beta&t=1og6ktPgeEYWkUywezFUf1F-B7eMgB_YFgXHOSZwbds',
+        ImageUrl      : 'https://media.licdn.com/dms/image/v2/D4D03AQFI-EkMrmiLzA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699030913771?e=1736985600&v=beta&t=1og6ktPgeEYWkUywezFUf1F-B7eMgB_YFgXHOSZwbds'
+    },
+);
--- app/project2/webapp/ext/main/Main.controller.js
+++ app/project2/webapp/ext/main/Main.controller.js
@@ -1,11 +1,73 @@
 sap.ui.define(
     [
-        'sap/fe/core/PageController'
+        'sap/fe/core/PageController',
+        'sap/ui/model/json/JSONModel',
+        'sap/m/MessageToast'
     ],
-    function(PageController) {
+    function(PageController, JSONModel,MessageToast) {
         'use strict';
 
         return PageController.extend('project2.ext.main.Main', {
+
+            onAfterRendering: function (oEvent) {
+				var oView = this.getView();
+				var mFBConditions = new JSONModel({
+					allFilters: "",
+					expanded: false,
+					filtersTextInfo: oView.byId("FilterBar").getActiveFiltersText()
+				});
+				oView.setModel(mFBConditions, "fbConditions");
+			},
+			handlers: {
+				onFiltersChanged: function (oEvent) {
+					var oView = this.getView();
+					var filterBar = oView.byId("FilterBar");
+					var allFilters = filterBar.getFilters();
+
+					var oSource = oEvent.getSource();
+					var mFBConditions = oSource.getModel("fbConditions");
+					mFBConditions.setProperty("/allFilters", JSON.stringify(allFilters, null, "  "));
+
+					if (Object.keys(allFilters).length > 0) {
+						mFBConditions.setProperty("/expanded", true);
+					}
+					MessageToast.show("FilterBar filters are changed!");
+					mFBConditions.setProperty("/filtersTextInfo", oSource.getActiveFiltersText());
+				},
+                onTilePress: function(oEvent){
+                    MessageToast.show('Tile was Pressed.. ***Can be used to filter probably***')
+                }
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             /**
              * Called when a controller is instantiated and its View controls (if available) are already created.
              * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
